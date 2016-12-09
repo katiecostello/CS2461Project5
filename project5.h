0a0054f551b49011f2df7ff817c968a1b506499c
katiecostello <katiecostello@gwu.edu>
2016-12-08 23:23:55 -0500

    header file

---
+++ project5.h
@@ -0,0 +1,30 @@
+
+struct hash{
+  int length;
+  struct bucket* table[20];
+  
+};
+
+struct bucket
+{
+  struct Bnode* head;
+};
+
+typedef struct bucket Bucket;
+
+struct Bnode {
+  char* word;               //word in node
+  int IDnum;                //ID number of the document in which the word is found
+  int times;                //number of times it appears in document
+  struct Bnode* next;       //node after this one in the bucket
+};
+
+// struct bucket* createBucket();
+
+// int hash_code(struct hash* b, char* w)
+
+// void hash_table_insert(struct hash* btable, char* w, int i);
+
+// int parse_list(struct bucket* b, struct Bnode* n);
+
+// void print_bucket(struct bucket *list);
